b38d48125a8490f586e84c383776bcd9cfd8bff4
sebastianlangbein <109713457+sebastianlangbein@users.noreply.github.com>
2022-09-15 13:40:57 +1000

    keybinds finish

--- Assets/Game systems/Scripts/Manager/KeyBindManager.cs
+++ Assets/Game systems/Scripts/Manager/KeyBindManager.cs
@@ -18,6 +18,11 @@ public class KeyBindManager : MonoBehaviour
     public KeybindUI[] inputKeys;
     public GameObject keyPrefab;
     public GameObject keyContainer;
+    private GameObject _currentInput;
+    public Color32 changedColour = new Color32(39, 171, 249, 255);
+    public Color32 selectedColour = new Color32(239, 116, 36, 255);
+    [SerializeField]
+    public Dictionary<string, KeyCode> keyBindDict = new Dictionary<string, KeyCode>();
 
     private void Start()
     {
@@ -33,11 +38,45 @@ public class KeyBindManager : MonoBehaviour
             }
             inputKeys[i].labelName.text = inputKeys[i].inputName;
             inputKeys[i].keyDisplay.text = inputKeys[i].keyDefaultValue;
+            keyBindDict.Add(inputKeys[i].inputName, (KeyCode)Enum.Parse(typeof(KeyCode), inputKeys[i].keyDefaultValue));
         }
     }
 
     public void ChangeKey(GameObject clickedKey)
     {
+        _currentInput = clickedKey;
+        if (clickedKey != null)//if we have clicked a keybind button and its registered that its selected
+        {
+            //change the colour to show the user that it is selected
+            _currentInput.GetComponent<Image>().color = selectedColour;
+        }
+    }
 
+    private void OnGUI()//allows events to run
+    {
+        string newKey = "";
+        Event e = Event.current;
+        if (_currentInput != null)
+        {
+            if (e.isKey)
+            {
+                newKey = e.keyCode.ToString();
+            }
+            //if (Input.GetKey(KeyCode.LeftShift))
+            //{
+            //    newKey = "LeftShift";
+            //}
+            //if (Input.GetKey(KeyCode.RightShift))
+            //{
+            //    newKey = "RightShift";
+            //}
+            if (newKey != "")//if we set a key
+            {
+                keyBindDict[_currentInput.name] = (KeyCode)Enum.Parse(typeof(KeyCode), newKey);
+                _currentInput.GetComponentInChildren<Text>().text = newKey;
+                _currentInput.GetComponent<Image>().color = changedColour;
+                _currentInput = null;
+            }
+        }
     }
 }
